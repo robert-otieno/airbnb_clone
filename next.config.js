57203962a8ae5af4cb9d405412d13782caf7e1c1
Robert Otieno <robertotieno.or@gmail.com>
2022-05-10 10:09:17 +0300

    Airbnb - Nextjs Frontend

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
 module.exports = {
-  reactStrictMode: true,
+  images: {
+    domains: ["links.papareact.com"]
+  }
 }
